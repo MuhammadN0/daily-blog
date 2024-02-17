2d60cebb292adc79683e1c36007de3873143ce57
Muhammad Noor <117872290+MuhammadN0@users.noreply.github.com>
2024-02-17 22:39:53 +0200

    finished requiring auth and no auth functions and added them to the available routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,49 @@ import HomeView from '../views/HomeView.vue'
 import NewArticleView from '@/views/NewArticleView.vue'
 import ThankYouView from '@/views/ThankYouView.vue'
 import AuthenticationView from '@/views/AuthenticationView.vue'
+import { auth } from '@/includes/firebase'
 
+function requireAuth(to,from,next) {
+  let user = auth.currentUser
+  if(!user){
+    next({name:'authentication'})
+  } else {
+    next();
+  }
+}
+function requireNoAuth(to,from,next){
+  let user = auth.currentUser
+  if(!user){
+    next()
+  } else {
+    next({name:'home'})
+  }
+}
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/new-post',
       name:'new-post',
       component:NewArticleView,
+      beforeEnter:requireAuth,
     },
     {
       path: '/thank-you',
       name: 'thank-you',
       component: ThankYouView,
+      beforeEnter:requireAuth,
     },
     {
       path: '/authentication',
       name: 'authentication',
-      component: AuthenticationView
+      component: AuthenticationView,
+      beforeEnter: requireNoAuth
     }
   ]
 })
